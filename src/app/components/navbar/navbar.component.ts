419d07324f2ae52a613bcb67b76b9d8c78f347ba
Justyna <justyna.kurcz1@gmail.com>
2024-11-26 18:59:31 +0100

    Update: UI styles for improved layout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from "@auth0/angular-jwt";
-import {NgIf} from "@angular/common";
+import { MenubarModule } from "primeng/menubar";
+import { MenuItem } from "primeng/api";
+import "primeicons/primeicons.css";
+import { MegaMenuModule } from "primeng/megamenu";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    NgIf
+    MenubarModule,
+    MegaMenuModule
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   private jwtHelper = new JwtHelperService();
+  items: MenuItem[] | undefined;
 
   logout(): void {
     localStorage.removeItem('token');
@@ -26,5 +31,42 @@ export class NavbarComponent {
       return false;
     }
     return !this.jwtHelper.isTokenExpired(token);
+  }
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Strona główna',
+        icon: 'pi pi-home',
+        routerLink: '/'
+      },
+      {
+        label: 'Dodaj zwierzaka',
+        icon: 'pi pi-plus',
+        routerLink: '/pet/create',
+        visible: this.isLoggedIn()
+      },
+      {
+        label: 'Kontakt',
+        icon: 'pi pi-envelope'
+      },
+      {
+        label: 'Justyna Kurcz',
+        icon: 'pi pi-user',
+        visible: this.isLoggedIn(),
+        items: [
+          {
+            label: 'Moje konto',
+            icon: 'pi pi-user',
+            routerLink: '/account'
+          },
+          {
+            label: 'Wyloguj',
+            icon: 'pi pi-sign-out',
+            command: () => this.logout(),
+          }
+        ]
+      }
+    ];
   }
 }
--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
-
 }
