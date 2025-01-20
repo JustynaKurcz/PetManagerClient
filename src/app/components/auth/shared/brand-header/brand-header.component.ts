5acef680de8625732bb6d038d15f893b2794d286
JustynaKurcz <35213@student.anstar.edu.pl>
2025-01-20 09:54:33 +0100

    refactor code

--- src/app/components/auth/shared/brand-header/brand-header.component.ts
+++ src/app/components/auth/shared/brand-header/brand-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-brand-header',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './brand-header.component.css'
 })
 export class BrandHeaderComponent {
-
+  @Input() brandName: string= '';
 }
